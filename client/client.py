cab6fc14c86ec0b33396dbcee5d384902d043de7
Aman13 <sandhu.amandeep.s@gmail.com>
2015-03-09 19:32:26 -0700

    Final working client and server. Takes user variables for Ip, host, and Command.

--- client/client.py
+++ client/client.py
@@ -1,24 +1,35 @@
 import sys
 from socket import *
 
+#User inputed arguments to specify host, port, file name and request type
 host = sys.argv[1]
 port = int(sys.argv[2])
 filename = sys.argv[3]
 requestType = sys.argv[4]
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((host,port))
 
+#connects to port given by user arguments
+clientSocket.connect((host,port))
+#sends the request in the proper format
 clientSocket.send(requestType + ' ' + filename)
 data = clientSocket.recv(1024)
+#outputs the servers response
 print data
-if (requestType == 'GET'):
+#Need to check the response to see if there is a file to download
+fileExist = data.split()[1]
+if (requestType == 'GET' and fileExist == '200'):
+	#creates a new file to download the requested file
+	newFile = open ('download_' + filename[1:], 'wb')
 	pageRequested = clientSocket.recv(1024)
 	recieving = True
+	#loop to make sure all the information is collected
 	while recieving == True:
 		raw = clientSocket.recv(1024)
 		pageRequested = pageRequested + raw
 		if raw == '':
 			recieving = False
+	#prints the information recieved on GET requests
 	print pageRequested
+	newFile.write(pageRequested)
 
 clientSocket.close()
--- server/server.py
+++ server/server.py
@@ -1,15 +1,19 @@
+
+import sys
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
+#Sets Host and port depending on usr arguments
+host = sys.argv[1]
+port = int(sys.argv[2])
 
-host = '127.0.0.1'
-port = 3000
-
+#binds the socket to the correct host:port only listens to one connection at a time
 serverSocket.bind((host,port))
 serverSocket.listen(1)
 
 while True:
 	print "Server Started"
 	connectionSocket, addr = serverSocket.accept()
+	#Once accepted a connection, parses information and looks for correct file
 	try:
 		message = connectionSocket.recv(1024)
 		requestType = message.split()[0]
@@ -17,11 +21,12 @@ while True:
 		f = open(filename[1:])
 		outputdata = f.read()
 		connectionSocket.send('HTTP/1.1 200 OK\n\n')
+		#If the request is a GET must send the necessary information
 		if (requestType == 'GET'):
 			for i in range(0, len(outputdata)):
 				connectionSocket.send(outputdata[i])
 		connectionSocket.close()
-
+	#if the file is not found, send the correct message and close the socket
 	except IOError:
 		connectionSocket.send('HTTP/1.1 404 Not found\n\n')
 		connectionSocket.close()
